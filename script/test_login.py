653688216a64e46b63e61e1f0d38e73455c8c5f2
lq <lq@163.com>
2018-12-24 17:52:41 +0800

    aaa

---
+++ script/test_login.py
@@ -0,0 +1,13 @@
+import allure,pytest
+
+class TestLogin:
+    def test_a(self):
+        print("\n111")
+        assert 1
+    @allure.step("测试登录的步骤")
+    def test_b(self):
+        print("\n222")
+        allure.attach("描述1","请输入用户名")
+        print("\n3333")
+        allure.attach("描述2", "请输入密码")
+        assert 0
